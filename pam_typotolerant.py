bc9b6d7b97088f93b83db4d210e92f70a293a9c0
Rahul Chatterjee <rahul@zenbook.com>
2016-09-05 14:17:31 -0400

    Now pam_typotolerant is statefulgst! (#IWasSoDumb)

--- pam_typotolerant.py
+++ pam_typotolerant.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import crypt
 import pwd
 import os, sys
@@ -9,39 +10,39 @@ from adaptive_typo import typo_db_access
 CHKPW_EXE = '/usr/local/bin/chkpw' # hardcoded path
 NN = 5 # hash cache's size
 
-# sys.path.insert(0, module_path)))
-# print sys.path
-# from typofixer.checker import BUILT_IN_CHECKERS
-# mychecker = BUILT_IN_CHECKERS['ChkBl_keyedit']
+DEBUG=1
+def eprint(*args, **kwargs):
+    if DEBUG:
+        print(*args, file=sys.stdout, **kwargs)
+    else:
+        print(*args, file=sys.stderr, **kwargs)
+
 
 def get_user(pamh, flags, argv):
     # getting username
     try:
         user = pamh.get_user(None)
     except pamh.exception, e:
-        print "Could not determine user.", e.pam_result
+        eprint("Could not determine user. {}".format(e.pam_result))
         return e.pam_result
     user = user.lower()
     try:
         pwdir = pwd.getpwnam(user)
     except KeyError, e:
-        print "Cound not fid user:", e
+        eprint("Cound not fid user:", e)
         return pamh.PAM_USER_UNKNOWN
     return user, pwdir
 
 def get_password(tmpPrompt, pamh, flags, argv):
-    password_prompt = tmpPrompt+";DB pASSWORD:"
+    password_prompt = tmpPrompt
     # getting password
     if pamh.authtok:
-        print "There is a authtok. Don't know what to do with it.", pamh.authtok
+        eprint("There is a authtok. Don't know what to do with it.", pamh.authtok)
     msg = pamh.Message(pamh.PAM_PROMPT_ECHO_OFF, password_prompt)
     resp = pamh.conversation(msg)
+    password = ''
     if not resp.resp_retcode:
         password = resp.resp
-
-    if (not password and \
-            (pamh.get_option ('nullok') or (flag & pamh.PAM_DISALLOW_NULL_AUTHTOK))):
-        return pamh.PAM_AUTH_ERROR
     return 'pw', password
 
 def fix_typos(pw):
@@ -56,7 +57,8 @@ def fix_typos(pw):
             pw.lower(),
             pw.upper()
         ])
-    ret = Top5Corrector(pw)
+    # if allow old top5 corrections
+    ret = Top5Corrector(pw) # right now IGORING.
     ret.add(pw) # Ensure the original `pw` always
     #return ret
     return [pw]
@@ -71,59 +73,68 @@ def check_pw(user, pws):
     except OSError:
         return -1
     # with open('/etc/typos-pm_sm_auth.txt', 'a') as f:
-    #     print "Writing to the file: before chek_pw"
+    #     eprint("Writing to the file: before chek_pw")
     #     f.write('user: {}, pw: {}, ts: {}\n'.format(user, pws,
-    #                                                                             datetime.datetime.now()))
+    #     datetime.datetime.now()))
     #     f.write('Return Code: {}'.format(p.returncode))
-    # print ''.join(p.stdout.readlines())
+    # eprint(''.join(p.stdout.readlines()))
     return p.returncode
 
+def on_correct_password(typo_db, password):
+    eprint("sm_auth: it's the right password") #TODO REMOVE
+    # log the entry of the original pwd
+    if not typo_db.is_typotoler_init():
+        eprint("sm_auth: initiating typoToler") # TODO REMOVE
+        typo_db.init_typotoler(password, NN)
+    typo_db.original_password_entered(password) # also updates the log
+    return True
+
+
+def on_wrong_password(typo_db, password):
+    t_sk, t_id, is_in = typo_db.fetch_from_cache(password) # also updates the log
+    if not is_in: # aka it's not in the cache, 
+        # maybe we should change for a more simple check, what if
+        # negative t_id is returned ?
+        eprint("sm_auth: a new typo!") # TODO REMOVE
+        typo_db.add_typo_to_waitlist(password)
+        return False
+    else: # it's in cach
+        eprint("sm_auth: in cach") # TODO REMOVE
+        typo_db.update_hash_cach_by_waitlist(t_id,t_sk) # also updates the log
+        if typo_db.is_typotoler_on():
+            eprint("Returning SUCEESS TypoToler")
+            return True
+        else:
+            eprint("sm_auth: but typoToler is OFF") # TODO REMOVE
+            return False
+
+
 def pam_sm_authenticate(pamh, flags, argv):
-    print "** Typo-tolerant password checking!"
-    print "** Typo-DB is ON ** "
+    eprint("** Typo-tolerant password checking!")
+    eprint("** Typo-DB is ON ** ")
     
     ret = get_user(pamh, flags, argv)
     if isinstance(ret, tuple) and len(ret) != 2:
         return ret
     user, pwdir = ret
-    typoDB = typo_db_access.UserTypoDB(user)
-    ret = get_password(str(typoDB),pamh, flags, argv) #tmp
-    if isinstance(ret, tuple) and len(ret) != 2 and ret[0] != 'pw':
-        return ret
-    _, password = ret
+    typo_db = typo_db_access.UserTypoDB(user)
 
-    # changes from now forth
-    
-    
-    if check_pw(user, password) == 0: #i.e - it's the password!
-        print "sm_auth: it's the right password" #TODO REMOVE
-        # log the entry of the original pwd
-        if not typoDB.is_typotoler_init():
-            print "sm_auth: initiating typoToler" # TODO REMOVE
-            typoDB.init_typotoler(password,NN)
-        typoDB.original_password_entered(password) # also updates the log
-        # TODO
-        
-        print "Returning SUCEESS"
-        return pamh.PAM_SUCCESS
-    else:
-        # it's a typo
-        t_sk,t_id,is_in = typoDB.fetch_from_cache(password) # also updates the log
-        if not is_in: # aka it's not in the cach
-            # maybe we should change for a mroe simple check
-            # negative t_id in return ?
-            print "sm_auth: a new typo!" # TODO REMOVE
-            typoDB.add_typo_to_waitlist(password)
-            return pamh.PAM_AUTH_ERR
-        # it's in cach
-        print "sm_auth: in cach" # TODO REMOVE
-        typoDB.update_hash_cach_by_waitlist(t_id,t_sk) # also updates the log
-        if typoDB.is_typotoler_on():
-            print "Returning SUCEESS TypoToler"
+    ATTEMPT_LIMIT = 3 # Maximum number of attempt allowed
+    for _ in range(ATTEMPT_LIMIT):
+        ret = get_password('aDAPTIVE pASSWORD: ', pamh, flags, argv)
+        if isinstance(ret, tuple) and len(ret) != 2 and ret[0] != 'pw':
+            return pamh.PAM_AUTH_ERR # Should never happen
+        _, password = ret
+        iscorrect = False
+        if check_pw(user, password) == 0: # i.e - it's the password!
+            iscorrect = on_correct_password(typo_db, password)
+        else:
+            iscorrect = on_wrong_password(typo_db, password)
+
+        if iscorrect:
             return pamh.PAM_SUCCESS
-        # else
-        print "sm_auth: but typoToler is OFF" # TODO REMOVE
-        return pamh.PAM_AUTH_ERR
+    return pamh.PAM_ERROR
+
 
 def pam_sm_setcred(pamh, flags, argv):
     return pamh.PAM_SUCCESS
@@ -142,4 +153,4 @@ def pam_sm_chauthtok(pamh, flags, argv):
 
 
 if __name__ == "__main__":
-    print check_pw('rahul', 'KiJataSob')
+    eprint(check_pw('rahul', 'KiJataSob'))
